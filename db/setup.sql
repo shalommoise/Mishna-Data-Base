302b77dbe1dc7e711d8693c78e3a8273122278e1
shalommoise <shalommoise@github.com>
2021-06-16 17:46:53 +0100

    created mishnayos table

--- db/setup.sql
+++ db/setup.sql
@@ -22,5 +22,27 @@ VALUES
   ('Nashim', 'נשים'),
   ('Nezikin', 'נזיקין'),
   ('Zevachim', 'זבחים'),
-  ('Taharos', 'טהרות')
+  ('Taharos', 'טהרות');
 
+CREATE TABLE masechta_table
+(
+  masechta_id SERIAL PRIMARY KEY,
+  masechta_name VARCHAR,
+  masechta_name_he VARCHAR,
+  seder_id INT,
+  perakim_number NUMERIC,
+  mishnayos_number NUMERIC,
+  FOREIGN KEY(seder_id) REFERENCES sedarim_table(seder_id)
+);
+
+
+CREATE TABLE mishna_table
+(
+  mishna_id SERIAL PRIMARY KEY,
+  perek_number NUMERIC,
+  mishna_number NUMERIC,
+  masechta_id INT,
+  mishna_text_he VARCHAR,
+  mishna_text_eng VARCHAR,
+ FOREIGN KEY(masechta_id) REFERENCES masechta_table(masechta_id)
+);
