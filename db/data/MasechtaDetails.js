3b05acef1ad016ce689dcd10dc23250172064782
shalommoise <shalommoise@github.com>
2021-06-17 16:35:14 +0100

    setting up get from sefaria mishna index

---
+++ db/data/MasechtaDetails.js
@@ -0,0 +1,9 @@
+// use https://www.sefaria.org/api/index/Mishna%20Berakhot to get info of mishna index
+// which masechta, how many perakim and mishnayos => lengths,
+//heTitle
+/* find which seder => "categories": [
+"Mishnah",
+"Seder Zeraim"
+],*/
+
+const sefariaAPILink = "https://www.sefaria.org/api/";
