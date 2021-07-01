1e9bc6786488ec4bc7bf8580255d46645ccb2b65
shalommoise <shalommoise@github.com>
2021-07-01 21:37:44 +0100

    seeded masechta details

--- db/seed/seed.js
+++ db/seed/seed.js
@@ -6,31 +6,28 @@ const {changeSederTitle } = require("../utils/utlis");
 
 
 
-const addMasechtaDetails = (masechtos, n)=>{ 
-
-     const {masechtaId,masechtaName, masechtaNameHe, sederName, perakimNumber, mishnayosNumber } = masechtos[n]
+const addMasechtaDetails = ()=>{    
+    
     return pool.connect()
-       .then(()=>client.query(`INSERT INTO masechta_table 
+       .then(()=>{
+           const  insertDetails = (masechtaDetails, n)=>  {
+            const {masechtaId,masechtaName, masechtaNameHe, sederName, perakimNumber, mishnayosNumber } = masechtaDetails[n];     
+    return   pool.query(`INSERT INTO masechta_table 
        (masechta_id, masechta_name, masechta_name_he, seder_name, number_of_perakim, number_of_misnayos) 
        VALUES
-       (${masechtaId} ,'${masechtaName}', '${masechtaNameHe}', '${changeSederTitle[sederName]}', ${perakimNumber}, ${mishnayosNumber});`))
+       (${masechtaId} ,'${masechtaName}', '${masechtaNameHe}', '${changeSederTitle[sederName]}', ${perakimNumber}, ${mishnayosNumber});`)
        .then(()=>{
            console.log(`added ${masechtaName}`);   
-           client.release()
-           return  n < masechtos.length && addMasechtaDetails(masechtos, n + 1);          
-        }).catch((err)=>{
-            console.log(err)
-        })
+           return  n < masechtaDetails.length - 1 && insertDetails(masechtaDetails, n + 1); 
+        })}
+        insertDetails(masechtaDetails, 0)
+    }).finally(()=> client.end())
+       
 }
 
 
-addMasechtaDetails(masechtaDetails, 0);
-// addMasechtaDetails(masechtaDetails, 9);
-// addMasechtaDetails(masechtaDetails, 18);
-// addMasechtaDetails(masechtaDetails, 27);
-// addMasechtaDetails(masechtaDetails, 36);
-// addMasechtaDetails(masechtaDetails, 45);
-// addMasechtaDetails(masechtaDetails, 54);
+addMasechtaDetails();
+
 
 
 
