4d2af88779de50b940a1ecf2b4ba949c6f184114
shalommoise <shalommoise@github.com>
2021-07-02 17:41:10 +0100

    fully seeded sedarim_table  and masechta_table

--- db/seed/seed.js
+++ db/seed/seed.js
@@ -6,7 +6,7 @@ const {changeSederTitle, reorderMasechtaArray } = require("../utils/utlis");
 
 const reorderedMasechtos = reorderMasechtaArray(masechtaDetails, "masechtaId");
 
-const addMasechtaDetails = ()=>{    
+const runSeed = ()=>{    
     
     return pool.connect()
        .then(()=>{
@@ -18,15 +18,47 @@ const addMasechtaDetails = ()=>{
        (${masechtaId} ,'${masechtaName}', '${masechtaNameHe}', '${changeSederTitle[sederName]}', ${perakimNumber}, ${mishnayosNumber});`)
        .then(()=>{
            console.log(`added ${masechtaName}`);   
-           return  n < reorderedMasechtos.length - 1 && insertDetails(reorderedMasechtos, n + 1); 
+           return  n < reorderedMasechtos.length - 1 ? insertDetails(reorderedMasechtos, n + 1) : updateSederTable() ; 
         })}
         insertDetails(reorderedMasechtos, 0)
-    }).finally(()=> client.end())
+    })
+    .finally(()=> client.end())
        
 }
 
+const updateSederTable = ()=>{
+    const sedraim = Object.values(changeSederTitle)
+    return pool.connect()
+    .then(()=>{
+       const updateSingleSeder =(n)=>{    
+           console.log(n)  
+        return pool.query(`SELECT * FROM masechta_table WHERE seder_name ='${sedraim[n]}';`)
+        .then((res)=>{
 
-addMasechtaDetails();
+         const numberOfMasechtos = res.rows.length;
+         let mishnayosNumber = 0;
+         let perakimNumber = 0;
+         res.rows.forEach((masechta)=>{
+         const { number_of_perakim, number_of_misnayos}= masechta;
+         mishnayosNumber += number_of_misnayos ;
+         perakimNumber += number_of_perakim;
+         })
+         return pool.query(`UPDATE sedarim_table SET number_of_masechtos=${numberOfMasechtos}, number_of_perakim=${perakimNumber}, number_of_misnayos=${mishnayosNumber} WHERE seder_name ='${sedraim[n]}';`)
+         .then(()=>{
+             console.log(`updated ${Object.values(changeSederTitle)[n]}`)
+             return n < sedraim.length - 1 && updateSingleSeder(n + 1);
+         })
+        })
+       }
+       updateSingleSeder(0)
+})
+
+}
+
+runSeed();
+
+
+
 
 
 
